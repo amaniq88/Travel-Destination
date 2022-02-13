6f7ecb2be49bc384e37c4b5bc59ea82cc7b50211
amaniq88 <amani.musallamq88@gmail.com>
2022-02-13 21:40:47 +0200

    Tsak completed

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+function Footer(){
+    return(
+        <>
+        <li>follow us on FaceBook www.FaceBook.com</li>
+        <li>follow us on Twetter  www.Twetter.com</li>
+        <li>follow us on Instagram  www.Instagram.com</li>
+        <br></br>
+        <li>Auther : Amani Musallam </li>
+
+
+
+        </>
+    )
+}
+export default Footer;
